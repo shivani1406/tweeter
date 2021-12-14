c86fe817495aa431ea9e79695b4a54fbb331a87d
Shivani Sharma <shivani1406sharma@gmail.com>
2021-12-14 22:42:14 +0000

    stylise tweets

--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -1,6 +1,11 @@
 function Count() {  
   
-  var i = document.getElementById("tweet-text").value.length;  
+  let i = document.getElementById("tweet-text").value.length; 
+  let output =  document.getElementById("counter");
   document.getElementById("counter").innerHTML = 140 - i;  
-
+  if ((document.getElementById("counter").innerHTML) < 0) {
+    output.classList.add("red");
+  } else {
+    output.classList.remove("red");
+  }
 }  
